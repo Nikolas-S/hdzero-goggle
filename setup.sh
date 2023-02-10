ac187a40d93bebd7fb31bac5aae032595cbab88a
Nikolas Saf <nikolas.saf@gmail.com>
2023-02-10 08:21:33 +0100

    fix architecture detection

--- setup.sh
+++ setup.sh
@@ -4,10 +4,12 @@
 
 architecture=$(arch)
 
-if [[ $architecture == *"aarm64"* ]]; then
+if [[ $architecture == *"arm64"* ]]; then
   echo "Starting setup for arm64 host..."
+  . ./setup_arm.sh
 elif [[ $architecture == *"x86"* ]]; then
   echo "Starting setup for x86 host..."
+  . ./setup_x86.sh
 else
   echo "Host architecture not supported: $architecture"
 fi
